b3db5639136d8f37a2adbd74b6b38fa87d096200
laura <laura.robinson@live.com>
2015-09-16 12:14:52 -0600

    index file

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+		<title>Laura Robinson</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width" />
+		<meta name="description" content="golf league scores and rankings" />
+		<link type="text/css" rel="stylesheet" href="CSS/styles.css" />		
+		<!link type="text/css" rel="stylesheet href="http://mysite.du.edu/~lrobins2/Golf/CSS/styles.css"/>
+ 		
+</head>
+
+<header>
+<?php include 'header.php'; ?></header>
+               
+<body> 
+
+
+	
+<div class = "middle">
+ 
+		<table class="leaderboardsnap">
+			<caption class="caption">
+			<a href="Leaderboard.html"> Leaderboard </a></caption>
+		
+			<colgroup>
+				<col id="name"/>
+				<col id="rank"/>
+				<col id="ringers"/>		
+			</colgroup>
+				
+			<tr>
+				<th scope="col">Name</th>
+				<th scope="col">Rank</th>
+				<th scope="col">Ringers</th>	
+			</tr>
+		
+			<tr>
+				<td class="golfername"> <a href="Golfer.html"> Laura R</a></td>
+				<td> 12</td>
+				<td> 90 </td>						
+		</tr>
+		
+		<tr>
+			<td> Viv P</td>
+			<td>35</td>
+			<td> 99 </td>				
+		</tr>
+		
+		<tr>
+			<td> Mary P</td>
+			<td> 10</td>
+			<td> 88 </td>							
+			</tr>
+		<tr>
+			<td> Kathy G</td>
+			<td> 8</td>
+			<td> 91 </td>
+										
+		</tr>
+		<tr>
+			<td> Alice</td>
+			<td> 12</td>
+			<td> 90 </td>						
+		</tr>
+		
+		<tr>
+			<td> Dana</td>
+			<td>35</td>
+			<td> 99 </td>				
+		</tr>
+		
+		<tr>
+			<td> Judy</td>
+			<td> 10</td>
+			<td> 88 </td>							
+			</tr>
+		<tr>
+			<td> Janet</td>
+			<td> 8</td>
+			<td> 91 </td>
+										
+		</tr>
+		<tr>
+			<td> Stefanie</td>
+			<td> 12</td>
+			<td> 90 </td>						
+		</tr>
+		
+		<tr>
+			<td> Ann</td>
+			<td>35</td>
+			<td> 99 </td>				
+		</tr>
+		
+		<tr>
+			<td> Jenni</td>
+			<td> 10</td>
+			<td> 88 </td>							
+			</tr>
+		<tr>
+			<td> Kathy M</td>
+			<td> 8</td>
+			<td> 91 </td>
+										
+		</tr>
+		<tr>
+			<td> Patricia</td>
+			<td> 12</td>
+			<td> 90 </td>						
+		</tr>
+		
+		<tr>
+			<td> Leslie</td>
+			<td>35</td>
+			<td> 99 </td>				
+		</tr>
+		
+		<tr>
+			<td> Carole</td>
+			<td> 10</td>
+			<td> 88 </td>							
+			</tr>
+		<tr>
+			<td> Susan</td>
+			<td> 8</td>
+			<td> 91 </td>
+										
+		</tr>
+		</table>	
+
+ 
+ 	<div class="middlecolumn">
+		<p class="centerstory"> Winners - Week 3 </p>
+		<p class="centerstory"> Winners - Week 2 </p>
+		<p class="centerstory"> Winners - Week 1 </p>
+	</div>
+
+	<div class="thirdcolumn"> 
+		<img src="Images/calendar.jpg" class="calendar" />
+		<p> upcoming events here </p>
+	</div>
+</div>
+    
+    
+    <footer> 
+        <?php include 'footer.php'; ?>  
+    </footer>
+
+
+
