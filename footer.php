a9cca4b9add29ed37729791bd5c449aed00f5abc
laura <laura.robinson@live.com>
2015-09-16 12:15:48 -0600

    footer file

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+    <footer>
+	<div class = "links">
+            <ul>
+                <li> <a href="https://www.linkedin.com/in/robinsonlaura"><img src="images/In.png"  alt = "LinkedIn"/>  
+                    </li>
+                <li class="ssp"> <a href="http://www.smallsquarephotography.com">  <img src="images/SSP.jpg" alt = "USGA"/>  
+                    </li>
+                <li class="emaillink">    <img src="images/email.png"  alt = "Email"/>  </li>
+			
+			
+            </ul>
+	</div>
+    </footer>
+</body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Laura Robinson</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="description" content="golf league scores and rankings" />
+        <link type="text/css" rel="stylesheet" href="CSS/styles.css" />	
+        <link href='https://fonts.googleapis.com/css?family=Abel|Exo:400,500italic|Sintony:400,700|Marmelad' rel='stylesheet' type='text/css'>
+    </head>
+    
+    <body> 
+        <div class="top">				
+            <h1> laura robinson </h1>				
+                <div class="headertwo" >
+                    <ul> 
+                        <li class="contact">thought leader</li>
+                        <li class="contact">change agent</li>
+                         <li class="contact">creator</li>
+                         <li class="contact">teacher</li>
+                    </ul>
+                </div>
+			
+</body>
+</html>
+
