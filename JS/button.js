6e00efb58340ee70d3025fccdc0a2fca5c996cac
laura <laura.robinson@live.com>
2015-10-14 12:22:21 -0600

    new buttons

--- JS/button.js
+++ JS/button.js
@@ -1,12 +1,42 @@
 
-// button togle
+// button togle for logos on accomplishemnts page
 
-     var PWRbutton = document.getElementById('PWRbutton');
-                PWRbutton.addEventListener('click',function(evt) {
-                var itemlist = document.getElementById('itemlist');
+     //var PWRbutton = document.getElementById('PWRbutton');
+       //         PWRbutton.addEventListener('click',function(evt) {
+         //       var itemlist = document.getElementById('itemlist');
+           //     itemlist.classList.toggle('hidden');       
+             //  });
+
+   
+    var MXLbutton = document.getElementById('MXLbutton');
+                MXLbutton.addEventListener('click',function(evt) {
+                var itemlist = document.getElementById('MXLlist');
                 itemlist.classList.toggle('hidden');       
                });
 
    
+    var MCDbutton = document.getElementById('MCDbutton');
+                MCDbutton.addEventListener('click',function(evt) {
+                var itemlist = document.getElementById('MCDlist');
+                itemlist.classList.toggle('hidden');       
+               });
+
+    var CSCbutton = document.getElementById('CSCbutton');
+                CSCbutton.addEventListener('click',function(evt) {
+                var itemlist = document.getElementById('CSClist');
+                itemlist.classList.toggle('hidden');       
+               });
+
    
-   
+    var TBPbutton = document.getElementById('TBPbutton');
+                TBPbutton.addEventListener('click',function(evt) {
+                var itemlist = document.getElementById('TBPlist');
+                itemlist.classList.toggle('hidden');       
+               });
+
+    var AMXbutton = document.getElementById('AMXbutton');
+                AMXbutton.addEventListener('click',function(evt) {
+                var itemlist = document.getElementById('AMXlist');
+                itemlist.classList.toggle('hidden');       
+               });
+
